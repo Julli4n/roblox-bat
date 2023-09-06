139568213b77849f4ce1b3f1023e8e3210f66798
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-09-06 01:30:13 -0700

    back to false

--- src/hbaClient.ts
+++ src/hbaClient.ts
@@ -136,7 +136,7 @@ export class HBAClient {
             }
 
             const isSecureAuthenticationIntentEnabled = el.getAttribute("data-is-secure-authentication-intent-enabled") === "true";
-            const isBoundAuthTokenEnabled = true; // el.getAttribute("data-is-bound-auth-token-enabled") === "true";
+            const isBoundAuthTokenEnabled = el.getAttribute("data-is-bound-auth-token-enabled") === "true";
             const boundAuthTokenWhitelist = JSON.parse(el.getAttribute("data-bound-auth-token-whitelist")!).Whitelist.map((item: {
                 sampleRate: string;
             }) => ({
