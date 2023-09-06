665fa97800c12c89701cbec11af96b3c1a2f2592
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-09-06 09:56:56 -0700

    fix

--- src/utils/parseDOM.node.ts
+++ src/utils/parseDOM.node.ts
@@ -1,4 +1,8 @@
 // @ts-nocheck no
 export function parseDOM(text: string): Document {
+    if ("DOMParser" in globalThis) {
+        return new globalThis.DOMParser().parseFromString(text, "text/html");
+    }
+
     return new DOMParser(text).window.document;
 }
