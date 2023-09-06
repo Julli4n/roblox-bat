5e4e353878bc709c7b808465b72f5b40470a9884
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-09-06 16:57:26 +0000

    Build: (665fa97) fix

--- esm/src/utils/parseDOM.node.js
+++ esm/src/utils/parseDOM.node.js
@@ -1,5 +1,8 @@
 import * as dntShim from "../../_dnt.shims.js";
 // @ts-nocheck no
 export function parseDOM(text) {
+    if ("DOMParser" in dntShim.dntGlobalThis) {
+        return new dntShim.dntGlobalThis.DOMParser().parseFromString(text, "text/html");
+    }
     return new dntShim.DOMParser(text).window.document;
 }
--- script/src/utils/parseDOM.node.js
+++ script/src/utils/parseDOM.node.js
@@ -27,6 +27,9 @@ exports.parseDOM = void 0;
 const dntShim = __importStar(require("../../_dnt.shims.js"));
 // @ts-nocheck no
 function parseDOM(text) {
+    if ("DOMParser" in dntShim.dntGlobalThis) {
+        return new dntShim.dntGlobalThis.DOMParser().parseFromString(text, "text/html");
+    }
     return new dntShim.DOMParser(text).window.document;
 }
 exports.parseDOM = parseDOM;
