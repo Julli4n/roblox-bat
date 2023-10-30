3edbc5c7d7023ad75df42e13a41a28d82aff1657
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-10-30 01:34:01 -0700

    read from this.isAuthenticated

--- scripts/dnt.ts
+++ scripts/dnt.ts
@@ -12,7 +12,7 @@ await build({
     package: {
         name: "roblox-bat",
         description: "A Deno/NodeJS module to generate Roblox BAT tokens for extensions",
-        version: "0.3.2",
+        version: "0.3.3",
         homepage: "https://github.com/Julli4n/roblox-bat",
         author: "Julli4n",
         bugs: {
--- src/hbaClient.ts
+++ src/hbaClient.ts
@@ -348,7 +348,7 @@ export class HBAClient {
             return true;
         }
         const metadata = await this.getTokenMetadata();
-        if ((!includeCredentials || !metadata?.isAuthenticated)) {
+        if ((!includeCredentials || !(metadata?.isAuthenticated || this.isAuthenticated))) {
             return false;
         }
 
